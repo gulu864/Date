0f0bb4a2c28723bfb5bc2a4b787b09000cd91733
gulu864 <suva.kanta4u@gmail.com>
2024-11-30 14:30:06 +0530

    Add files via upload

---
+++ After_Calssproject.py
@@ -0,0 +1,13 @@
+from datetime import datetime,date
+import calendar
+date1 = date(2014,12,8)
+print(date1.strftime("%A"))
+
+date1 = date(2022,7,22)
+print(date1.strftime("%A"))
+
+date1 = date(1982,6,3)
+print(date1.strftime("%A"))
+
+date1 = date(1986,7,6)
+print(date1.strftime("%A"))
